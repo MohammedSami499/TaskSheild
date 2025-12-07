738040d7435f85220c31498badeba9d6c1dea176
msami <msami@kn-it.com>
2025-12-07 11:34:31 +0300

    feat: initial project setup

---
+++ src/test/java/com/samiCoding/task_sheild/TaskSheildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samiCoding.task_sheild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSheildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
